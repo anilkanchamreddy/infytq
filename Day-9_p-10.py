15c7bd252854cba3d7837550898da741e6f0ae28
Anil kumar.Kanchamreddy <73389571+anilkanchamreddy@users.noreply.github.com>
2020-11-05 13:59:16 +0530

    Create Day-9_p-10.py

---
+++ Day-9_p-10.py
@@ -0,0 +1,11 @@
+#PF-Prac-10
+def string_both_ends(input_string):
+    #start writing your code here
+    if len(input_string)<2:
+        result=-1
+    else:
+        result=input_string[:2]+input_string[-2:]
+    return result
+
+input_string="w3w"
+print(string_both_ends(input_string))
