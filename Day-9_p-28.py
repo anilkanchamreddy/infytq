6c5f3ef01301c67402bfdd1af0c80416ce744bc4
Anil kumar.Kanchamreddy <73389571+anilkanchamreddy@users.noreply.github.com>
2020-11-08 19:08:45 +0530

    Create Day-9_p-28.py

---
+++ Day-9_p-28.py
@@ -0,0 +1,8 @@
+#PF-Tryout
+def sing_99_bottles():
+    #start writing your code here
+    count=99
+    while count>=0:
+        print(count,'bottles of beer on the wall,',count,'bottles of beer')
+        count-=1
+sing_99_bottles()
